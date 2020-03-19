5bed3c1293b8f6b4ff20a929186dd5586e26ee3b
JuanSan1705 <60147407+JuanSan1705@users.noreply.github.com>
2020-03-19 14:15:21 -0500

    Update Operaciones.py

--- Operaciones.py
+++ Operaciones.py
@@ -58,3 +58,68 @@ def conjugado(c1):
 
 def Division(C1, R):
     return (C1[0]/R, C1[1]/R)
+
+def resta(c1,c2):
+    """Recibo 2 complejos y los resta -> complejo
+    """
+    a1, b1, a2, b2= c1[0], c1[1], c2[0], c2[1]
+    return (a1-a2, b1-b2)
+
+def transpuesta(m1):
+    """Recibo una matriz compleja y determino su transpuesta -> matriz compleja
+    """
+    m2=[[(0,0) for x in m1] for x in m1[0]]
+    for j in range(len(m1[0])):
+        for k in range(len(m1)):
+            m2[j][k]=m1[k][j]
+    return m2
+
+def matrizConjugada(m1):
+    """Recibo una matriz compleja y determino la matriz conjugada -> matriz compleja
+    """
+    for j in range(len(m1)):
+        for k in range(len(m1[0])):
+            m1[j][k]=conjugado(m1[j][k])
+    return m1
+
+def matrizAdjunta(m1):
+    """Recibo una matriz compleja y determino matriz adjunta -> matriz compleja
+    """
+    return matrizConjugada(transpuesta(m1))
+
+def matrizHermitian(m1):
+    """ Determino si una matriz es hermitian -> boolean
+    """
+    if (len(m1)!=len(m1[0])):
+        return "Imposible"
+    else:
+        esHermitian = True
+        m2 = matrizAdjunta(m1)
+        for j in range(len(m1)):
+            for k in range(len(m1)):
+                if (resta(m1[j][k],m2[j][k])!=(0,0)):
+                    esHermitian = False
+                    break
+        return esHermitian
+
+def matrizIdentidad(n):
+    """ Recibo un entero y determino la matriz identidad n*n -> matriz
+    """
+    ident=[[(0,0) for x in range(n)] for x in range(n)]
+    for j in range(n):
+        ident[j][j]=(1,0)
+    return ident
+
+def multiplicacionEscalarVector(v1,c):
+    """Recibo 1 vector complejo y un escalar y hago la multiplicacion escalar de vector -> vector complejo
+    """
+    for j in range(len(v1)):
+        v1[j]=multiplicacion(v1[j],c)
+    return v1
+
+def multiplicacionEscalarMatriz(m1,c):
+    """Recibo una matriz compleja y un escalar y hago la multiplicacion escalar de matriz -> matriz compleja
+    """
+    for j in range(len(m1)):
+        m1[j]=multiplicacionEscalarVector(m1[j],c)
+    return m1
