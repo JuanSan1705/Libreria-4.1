a4451c82899835b2296dd8190759214333acb996
JuanSan1705 <60147407+JuanSan1705@users.noreply.github.com>
2020-03-19 14:14:13 -0500

    Update Libreria.py

--- Libreria.py
+++ Libreria.py
@@ -1,4 +1,5 @@
 import Operaciones as Op
+import math
 
 
 def SumModulos(VectorKet, Posicion):
@@ -49,4 +50,37 @@ def AmplitudTrans(VectorKet, VectorKet2):
         for x in range (len(Result)):
             sum = Op.suma(sum, Result[x])
         return  sum
+    
+def CalculoMedia(Matriz, Ket):
+    if Op.matrizHermitian(Matriz) == True:
+        M = Op.multiplicacionMatrizVector(Matriz, Ket)
+        M1 = []
+        for h in range (len(M)):
+            M1.append(M[h][0])
+        kResult = []
+        for Cam in range(len(M1)):
+            kResult.append(Op.conjugado(M1[Cam]))
+        ket1 = []
+        for x in range(len(Ket)):
+            ket1.append(Ket[x][0])
+        Sum = (0, 0)
+        k = []
+        for i in range (len(Ket)):
+            Mult = Op.multiplicacion(kResult[i], ket1[i])
+            k.append(Mult)
+        for j in range(len(k)):
+            Sum = Op.suma(Sum, k[j])
 
+
+        Delta = Op.multiplicacionEscalarMatriz(Op.matrizIdentidad(len(Matriz)), Sum)
+        n = Matriz
+        for p in range(len(Matriz)):
+            for r in range(len(Matriz)):
+                n[p][r] = Op.resta(Matriz[p][r], Delta[p][r])
+        return n
+    else:
+        return 'Matriz no Hermitiana'
+
+def Variance(Matriz, Ket):
+    m = Op.multiplicacionMatrizMatriz(CalculoMedia(Matriz, Ket), CalculoMedia(Matriz, Ket))
+    return m
